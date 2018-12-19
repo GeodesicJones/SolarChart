f9b67008c7da959db6e56a71b3ed228b484c18bb
Mark <j.mark.boyd@gmail.com>
2018-12-19 11:48:10 -0800

    Fix create script for zipfile deploy of python code.

--- infrastructure/create-stack.ps1
+++ infrastructure/create-stack.ps1
@@ -32,80 +32,80 @@ Write-S3Object -BucketName $deployBucket `
     -File $zipPath `
     -Region $region
 
-# ####################################################
-# # Push stack template to deploy bucket and execute
-# ####################################################
+####################################################
+# Push stack template to deploy bucket and execute
+####################################################
 
-# function StackExists($stackName) {
-#     $stacks = Get-CFNStack -Region $region
-#     foreach ($stack in $stacks) {
-#         if ($stack.StackName = $stackName) {
-#             return true;
-#         }
-#     }
-#     return false;
-# }
+function StackExists($stackName) {
+    $stacks = Get-CFNStack -Region $region
+    foreach ($stack in $stacks) {
+        if ($stack.StackName = $stackName) {
+            return true;
+        }
+    }
+    return false;
+}
 
-# if (!(Test-S3Bucket  -BucketName $deployBucket)) {
-#     New-S3Bucket -BucketName $deployBucket -Region $region
-# }
+if (!(Test-S3Bucket  -BucketName $deployBucket)) {
+    New-S3Bucket -BucketName $deployBucket -Region $region
+}
 
-# Write-S3Object -BucketName $deployBucket `
-#     -Key $templateName `
-#     -File $templateName `
-#     -Region $region
+Write-S3Object -BucketName $deployBucket `
+    -Key $templateName `
+    -File $templateName `
+    -Region $region
 
-# if (!(StackExists $stackName)) {
-#     New-CFNStack `
-#         -StackName $stackName `
-#         -Region $region `
-#         -TemplateURL "https://s3.amazonaws.com/$deployBucket/$templateName" `
-#         -Capability CAPABILITY_IAM `
-#         -Parameters @( `
-#         @{ ParameterKey = 'AppSubDomain'; ParameterValue = $appSubDomain}, `
-#         @{ ParameterKey = 'AllowedOrigin'; ParameterValue = $allowedOrigin}, `
-#         @{ ParameterKey = 'HostedZone'; ParameterValue = $hostedZone}
-#     ) 
-#     Write-Output('Waiting for create...')
-#     Wait-CFNStack -StackName $stackName -Status CREATE_COMPLETE -Region $region -Timeout 7200
-#     Write-Output('Create Complete')
-# }
+if (!(StackExists $stackName)) {
+    New-CFNStack `
+        -StackName $stackName `
+        -Region $region `
+        -TemplateURL "https://s3.amazonaws.com/$deployBucket/$templateName" `
+        -Capability CAPABILITY_IAM `
+        -Parameters @( `
+        @{ ParameterKey = 'AppSubDomain'; ParameterValue = $appSubDomain}, `
+        @{ ParameterKey = 'AllowedOrigin'; ParameterValue = $allowedOrigin}, `
+        @{ ParameterKey = 'HostedZone'; ParameterValue = $hostedZone}
+    ) 
+    Write-Output('Waiting for create...')
+    Wait-CFNStack -StackName $stackName -Status CREATE_COMPLETE -Region $region -Timeout 7200
+    Write-Output('Create Complete')
+}
 
-# function UploadFile([string]$path, [string]$name, [string]$bucket) {
-#     Write-S3Object -BucketName $bucket `
-#         -Key $name `
-#         -File "$path/$name" `
-#         -Region $region
-# }
+function UploadFile([string]$path, [string]$name, [string]$bucket) {
+    Write-S3Object -BucketName $bucket `
+        -Key $name `
+        -File "$path/$name" `
+        -Region $region
+}
 
-# # Fix api paths in widget
-# if (!(Test-Path "./temp")) {
-#     New-Item -Path "." -Name "temp" -ItemType "directory"
-# }
-# Copy-Item "../app/blog-widget.html" -Destination "./temp"
-# $widgetContent = Get-Content -path './temp/blog-widget.html'  -Raw
-# $widgetContent = $widgetContent.Replace("data2016", "https://api-$appSubdomain/?key=data2016")
-# $widgetContent = $widgetContent.Replace("data2018", "https://api-$appSubdomain/?key=data2018")
-# Set-Content -Path './temp/blog-widget.html' -Value $widgetContent
+# Fix api paths in widget
+if (!(Test-Path "./temp")) {
+    New-Item -Path "." -Name "temp" -ItemType "directory"
+}
+Copy-Item "../app/blog-widget.html" -Destination "./temp"
+$widgetContent = Get-Content -path './temp/blog-widget.html'  -Raw
+$widgetContent = $widgetContent.Replace("data2016", "https://api-$appSubdomain/?key=data2016")
+$widgetContent = $widgetContent.Replace("data2018", "https://api-$appSubdomain/?key=data2018")
+Set-Content -Path './temp/blog-widget.html' -Value $widgetContent
 
-# Write-Host 'Pushing files...'
+Write-Host 'Pushing files...'
 
-# UploadFile './temp' 'blog-widget.html' $appBucket
-# UploadFile '../app' 'solarchart.js' $appBucket
-# UploadFile '../data' 'data2016.json' $dataBucket
-# UploadFile '../data' 'data2018.json' $dataBucket
+UploadFile './temp' 'blog-widget.html' $appBucket
+UploadFile '../app' 'solarchart.js' $appBucket
+UploadFile '../data' 'data2016.json' $dataBucket
+UploadFile '../data' 'data2018.json' $dataBucket
 
-# Write-Host 'Invalidating distro...'
+Write-Host 'Invalidating distro...'
 
-# $distroId = 0
-# $distros = Get-CFDistributionList -Region $region
-# foreach ($distro in $distros) {
-#     if ($distro.Aliases[0].Items = $appSubDomain) {
-#         $distroId = $distro.Id
-#     }
-# }
+$distroId = 0
+$distros = Get-CFDistributionList -Region $region
+foreach ($distro in $distros) {
+    if ($distro.Aliases[0].Items = $appSubDomain) {
+        $distroId = $distro.Id
+    }
+}
 
-# New-CFInvalidation -DistributionId $distroId -Region $region `
-#     -InvalidationBatch_CallerReference (get-date).ticks `
-#     -Paths_Item '/*' `
-#     -Paths_Quantity 1
+New-CFInvalidation -DistributionId $distroId -Region $region `
+    -InvalidationBatch_CallerReference (get-date).ticks `
+    -Paths_Item '/*' `
+    -Paths_Quantity 1
