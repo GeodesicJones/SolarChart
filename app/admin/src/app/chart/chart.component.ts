566718f82ed3acd4464162e60bf05a2fdc3c45d5
Mark <j.mark.boyd@gmail.com>
2019-01-02 14:50:23 -0800

    Change chart component to 2019 format.

--- app/admin/src/app/chart/chart.component.ts
+++ app/admin/src/app/chart/chart.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import * as moment from "moment";
-import { forkJoin } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { Chart } from "chart.js";
 import { DataService } from "../data.service";
@@ -13,7 +12,7 @@ import { DataService } from "../data.service";
 export class ChartComponent implements OnInit {
   constructor(private _dataService: DataService, private _http: HttpClient) {}
 
-  makeSeries(rawData) {
+  makeProductionSeries(rawData) {
     var seriesData = [];
     for (var i = 1; i < rawData.length; i++) {
       var currentDayOfYear = moment(rawData[i].date, "MM/DD/YYYY").dayOfYear();
@@ -36,7 +35,31 @@ export class ChartComponent implements OnInit {
     return seriesData;
   }
 
-  makeCharts(data2016, data2018) {
+  makeConsumptionSeries(rawData) {
+    var seriesData = [];
+    for (var i = 1; i < rawData.length; i++) {
+      var currentDayOfYear = moment(rawData[i].date, "MM/DD/YYYY").dayOfYear();
+      var previousDayOfYear = moment(
+        rawData[i - 1].date,
+        "MM/DD/YYYY"
+      ).dayOfYear();
+      var deltaDay = currentDayOfYear - previousDayOfYear;
+      var deltaDial = rawData[i].dial - rawData[i - 1].dial;
+      var deltaIn = rawData[i].code14 - rawData[i - 1].code14;
+      var deltaOut = rawData[i].code24 - rawData[i - 1].code24;
+      var consumed = deltaIn - deltaOut + deltaDial;
+      var averageDailyConsumption = consumed / deltaDay;
+
+      seriesData.push({
+        x: currentDayOfYear,
+        y: averageDailyConsumption
+      });
+    }
+
+    return seriesData;
+  }
+
+  makeCharts(dataProduced, dataConsumed) {
     Chart.defaults.global.elements.point.pointStyle = "triangle";
     new Chart("canvas", {
       responsive: true,
@@ -44,19 +67,19 @@ export class ChartComponent implements OnInit {
       data: {
         datasets: [
           {
-            label: "2018",
+            label: "Produced",
             fill: false,
-            data: data2018,
-            backgroundColor: ["rgba(79,129,189,1)"],
-            borderColor: ["rgba(79,129,189,1)"],
+            data: dataProduced,
+            backgroundColor: ["rgba(0,196,0,1)"],
+            borderColor: ["rgba(0,196,0,1)"],
             borderWidth: 1
           },
           {
-            label: "2016",
+            label: "Consumed",
             fill: false,
-            data: data2016,
-            backgroundColor: ["rgba(155,187,89,1)"],
-            borderColor: ["rgba(155,187,89,1)"],
+            data: dataConsumed,
+            backgroundColor: ["rgba(196,0,0,1)"],
+            borderColor: ["rgba(196,0,0,1)"],
             borderWidth: 1
           }
         ]
@@ -120,13 +143,10 @@ export class ChartComponent implements OnInit {
   }
 
   ngOnInit() {
-    forkJoin(
-      this._dataService.get("data2016"),
-      this._dataService.get("data2018")
-    ).subscribe(res => {
-      var series2016 = this.makeSeries(res[0]);
-      var series2018 = this.makeSeries(res[1]);
-      this.makeCharts(series2016, series2018);
+    this._dataService.get("data2019").subscribe(res => {
+      var seriesProduced = this.makeProductionSeries(res);
+      var seriesConsumed = this.makeConsumptionSeries(res);
+      this.makeCharts(seriesProduced, seriesConsumed);
     });
   }
 }
